cfab2dd392dbec0e334ca7e3be990f92e6b9290c
Hellen Bilek Dukievicz <145031037+Hellen2008@users.noreply.github.com>
2024-11-08 10:52:40 -0300

    virgulas

--- perguntas.js
+++ perguntas.js
@@ -1,9 +1,9 @@
 criaCartao(
     'Educação Física',
     ' O que é a frequência cardíaca?',
-    'A frequência cardíaca é o número de batimentos do coração por minuto. Durante atividades físicas, ela tende a aumentar, pois o corpo precisa de mais oxigênio.
+    'A frequência cardíaca é o número de batimentos do coração por minuto. Durante atividades físicas, ela tende a aumentar, pois o corpo precisa de mais oxigênio.',
 
-'
+
 )
 
 criaCartao(
@@ -15,15 +15,14 @@ criaCartao(
 criaCartao(
     'Programação',
     'O que é uma variável em programação?',
-    ' Uma variável é um espaço na memória do computador onde podemos armazenar valores temporários, como números ou texto, para serem usados durante a execução do programa.
+    ' Uma variável é um espaço na memória do computador onde podemos armazenar valores temporários, como números ou texto, para serem usados durante a execução do programa.',
 
-'
 )
 
 criaCartao(
     'Sociologia',
     ' O que são instituições sociais?',
-    'Instituições sociais são estruturas organizadas que desempenham funções essenciais na sociedade, como a família, a educação, a religião e o governo.
+    'Instituições sociais são estruturas organizadas que desempenham funções essenciais na sociedade, como a família, a educação, a religião e o governo.',
 
-'
+
 )

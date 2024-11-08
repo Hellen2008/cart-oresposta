c22d690bbf35ed30f6aafac34c99f8b7a1888b0d
Hellen Bilek Dukievicz <145031037+Hellen2008@users.noreply.github.com>
2024-11-08 09:51:26 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,15 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Educação Física',
+    ' O que é a frequência cardíaca?',
+    'A frequência cardíaca é o número de batimentos do coração por minuto. Durante atividades físicas, ela tende a aumentar, pois o corpo precisa de mais oxigênio.
+
+'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Inglês',
+    ' Qual a diferença entre "can" e "may" em inglês?',
+    ' "Can" é usado para falar sobre habilidade ou capacidade, enquanto "may" é usado para pedir permissão de forma mais formal.'
 )
 
 criaCartao(
